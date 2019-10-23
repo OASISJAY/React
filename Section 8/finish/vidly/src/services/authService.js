e0de14b92d2ce36c33a86a0a86c8eca68e62c60c
kenne@AF6TOS4 <kenneth.ji@outlook.com>
2019-10-23 13:39:23 +1100

    finally finished

---
+++ Section 8/finish/vidly/src/services/authService.js
@@ -0,0 +1,9 @@
+//responsible for login and out.
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+
+const apiEndpoint = apiUrl + "/auth";
+
+export function login(email, password) {
+    return http.post(apiEndpoint, { email, password });
+}
